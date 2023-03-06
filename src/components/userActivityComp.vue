e2d77bf014224b5742f195741e72c1899f3f00fd
Frederik Vilhelmsen <frederik@rovi.dk>
2023-03-06 08:53:25 +0100

    Fixed some shit

--- src/components/userActivityComp.vue
+++ src/components/userActivityComp.vue
@@ -8,25 +8,24 @@ export default {
     navigationbar,
     userComp,
     userPostComp,
-    userPostCommentComp
+    userPostCommentComp,
   },
-  data(){
-           return {
-              id: this.$route.params.id //this is the id from the browser
-           }
-       },
+  data() {
+    return {
+      id: Number.parseInt(this.$route.params.id as string), //this is the id from the browser
+    };
+  },
 };
 </script>
 
 <template>
-    <div class="container-fluid">
-        <navigationbar/>
-        <userComp id="1"/>
-        
-        <div class="row">
-          <div class="col-6"><userPostComp id="1"/></div>
-          <div class="col-6"><userPostCommentComp id="1"/></div>
-        </div>
+  <div class="container-fluid">
+    <navigationbar />
+    <userComp :id="id" />
+    <div class="row">
+      <div class="col-6"><userPostComp id="1" /></div>
+      <div class="col-6"><userPostCommentComp id="1" /></div>
+    </div>
   </div>
 </template>
 
--- src/components/userActivityComp/userDataComp.vue
+++ src/components/userActivityComp/userDataComp.vue
@@ -1,36 +1,34 @@
 <script lang="ts">
-import { useUserStore } from '@/stores/user'; 
+import { useUserStore } from "@/stores/user";
 export default {
-    props:["id"],
-    setup() {
-        const userStore = useUserStore();
-        return { userStore }
-    },
-    methods: {},
+  props: ["id"],
+  setup() {
+    const userStore = useUserStore();
+    return { userStore };
+  },
+  methods: {},
 };
 </script>
 
 <template>
-    <div class="row p-3">
-        <div class="col-12">
-            <div class="row p-2 bg-light shadow border">
-                <div class="col-3 text-center">
-                    <h3>Name: {{ userStore.getUserById(1)?.name }}</h3>
-                </div>
-                <div class="col-3 text-center">
-                    <h3>Email: {{ userStore.getUserById(1)?.email }}</h3>
-                </div>
-                <div class="col-3 text-center">
-                    <h3>Role: {{ userStore.getUserById(1)?.role.role }}</h3>
-                </div>
-                <div class="col-3 text-center">
-                    <h3>Created at: {{ userStore.getUserById(1)?.createdAt}}</h3>
-                </div>
-            </div>
+  <div class="row p-3">
+    <div class="col-12">
+      <div class="row p-2 bg-light shadow border">
+        <div class="col-3 text-center">
+          <h3>Name: {{ userStore.getUserById(id)?.name }}</h3>
         </div>
+        <div class="col-3 text-center">
+          <h3>Email: {{ userStore.getUserById(id)?.email }}</h3>
+        </div>
+        <div class="col-3 text-center">
+          <h3>Role: {{ userStore.getUserById(id)?.role.role }}</h3>
+        </div>
+        <div class="col-3 text-center">
+          <h3>Created at: {{ userStore.getUserById(id)?.createdAt }}</h3>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
-<style>
-
-</style>
+<style></style>
